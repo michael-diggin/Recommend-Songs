178197fffff7e26b730474b955040f0997aa8cae
Michael Diggin <michael.diggin@gmail.com>
2019-08-07 09:42:29 +0100

    add recommended tracks to playlist

--- RecommendEngine.py
+++ RecommendEngine.py
@@ -11,15 +11,25 @@ disc_weekly_features = spot.song_features(disc_weekly_songs)
 predictions = predictions(trained_model, disc_weekly_features)
 
 recommended = []
+rec_ids = []
 for i in range(len(disc_weekly_songs)):
     if predictions[i] >= 0.9:
         recommended.append(disc_weekly_songs[i]['track']['name'])
+        rec_ids.append(disc_weekly_songs[i]['track']['id'])
 
 if recommended is not None:
-    print('The follwoing songs are recommeneded based on your saved songs:')
+    print('The following songs are recommeneded based on your saved songs:')
     print('\n')
     for song in recommended:
         print(song)
 else:
     print('No songs matched your likes by over 80%')
+
+if rec_ids is not None:
+    spot.add_tracks(rec_ids, 'Recommended')
+
+
+
+
+
 
